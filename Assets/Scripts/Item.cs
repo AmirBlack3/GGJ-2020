94dde4d4e898a4cb8f732ff08d80aba9243a5b30
black3 <amir.nazari1375@gmail.com>
2020-01-30 16:30:07 +0330

    - movement - carry items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Player myPlayer { get; set; }
+    public GameObject pos { get; set; }
+    public bool isCarrying { get; set; }
+    public bool onDesk { get; set; }
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        Carrying();
+    }
+    void Carrying()
+    {
+        if (pos != null)
+            transform.position = pos.transform.position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int speed;
+    public Text interactTxt;
+    public GameObject hand;
+
+    private Item myItem;
+    private bool isDashing;
+
+    void Start()
+    {
+        interactTxt.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        Move();
+
+        Dash();
+
+        interactTxt.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+    }
+    void Move()
+    {
+        var lh = Input.GetAxis("Horizontal");
+        var lv = Input.GetAxis("Vertical");
+        var movement = new Vector3(lh * speed * Time.deltaTime, 0, lv * speed * Time.deltaTime);
+        transform.position += movement;
+
+        if (movement != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(movement);
+    }
+    void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            isDashing = true;
+
+        if (isDashing == true)
+            transform.position += Vector3.forward;
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Desk" || other.gameObject.tag == "Item")
+            interactTxt.gameObject.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (other.gameObject.tag == "Desk")
+            {
+                if (myItem != null)
+                {
+                    myItem.pos = other.gameObject.GetComponent<Desk>().top;
+                    //myItem = null;
+                }
+            }
+
+            if (other.gameObject.tag == "Item")
+            {
+                if (myItem == null)
+                {
+                    myItem = other.gameObject.GetComponent<Item>();
+                    myItem.pos = hand;
+                }
+                else
+                {
+                    if (myItem.pos == hand)
+                        myItem.pos = null;
+                    myItem = null;
+                }
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Item" || other.gameObject.tag == "Desk")
+            interactTxt.gameObject.SetActive(false);
+    }
+}
